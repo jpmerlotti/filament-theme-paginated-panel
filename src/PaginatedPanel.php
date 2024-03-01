12f884b55241379b3372893de43bfbe1a12dcdf4
João Pedro Merlotti <joaopedromerlotti@gmail.com>
2024-03-01 13:55:38 -0300

    feat/add-paginatin

--- src/PaginatedPanel.php
+++ src/PaginatedPanel.php
@@ -2,16 +2,18 @@
 
 namespace Jpmerlotti\PaginatedPanel;
 
-use Jpmerlotti\PaginatedPanel\Columns\TextColumn;
-use Jpmerlotti\PaginatedPanel\Columns\NumberColumn;
+use Illuminate\View\View;
 use Livewire\Component;
 use Livewire\WithPagination;
 
 abstract class PaginatedPanel extends Component
-
 {
 //    public abstract function query(): Builder;
     use withPagination;
+
+    public array $data = [];
+    public int $selectedPage = 1;
+
     public abstract function api(): array;
 
     public abstract function columns(): array;
@@ -20,18 +22,32 @@ abstract class PaginatedPanel extends Component
         return $this->api()['data'];
     }
 
-    public function paginate(): array
+    public function mount(): void
     {
-        return $this->api();
+        $this->paginate();
     }
-//    public function pagination()
-//    {
-//        $current_page = $this->api()['current_page'];
-//        $last_page = $this->api()['last_page'];
-//        return ['current_page' => $current_page, 'last_page' => $last_page];
-//    }
+    public function paginate(int $page = 1): void
+    {
+        $this->selectedPage = $page;
+        $this->data = $this->api($page);
+        $this->resetPage('page');
+    }
+
+    public function nextPage(int $page): void
+    {
+        $this->selectedPage = $page + 1;
+        $this->data = $this->api($page + 1);
+        $this->resetPage('page');
+    }
+    public function previousPage(int $page): void
+    {
+        $this->selectedPage = $page - 1;
+        $this->data = $this->api($page - 1);
+        $this->resetPage('page');
+    }
+
     public function render()
     {
-        return view('paginated-panel::livewire.table');
+        return view('PaginatedPanel::livewire.table');
     }
 }
